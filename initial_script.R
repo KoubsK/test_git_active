35071aca6b452ea4a7823edf76a302608e6541cb
Karel Koubský <koubskyka@natur.cuni.cz>
2024-09-26 15:10:10 +0200

    Removing private info

--- initial_script.R
+++ initial_script.R
@@ -6,7 +6,7 @@ usethis::use_git()
 
 usethis::create_from_github(
   repo_spec = "https://github.com/SSoQE/VersionControl-playground.git",
-  destdir = "C:/Users/koubs/Documents/R/Example",
+  destdir = "C:/Users/user/Documents/etc",
   fork = FALSE
 )
 
